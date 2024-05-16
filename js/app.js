8c8c649d80c3408f2162e78b3dceb2f2495abe44
Arruda Junior <juniorarruda15@gmail.com>
2024-05-16 14:48:26 -0300

    Obtenado os valores do id, imagem, botao e nome do jogo

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function alterarStatus(id) {
+    let jogoAlugado = document.getElementById(`Game-${id}`);
+    let imagem = jogoAlugado.querySelector('.dashboard__item__img');
+    let botao = jogoAlugado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoAlugado.querySelector('.dashboard__item__name');
+}
