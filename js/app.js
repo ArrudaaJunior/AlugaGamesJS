4e5d4b9205f83c7bd4148c2f9362dec43af64d12
Arruda Junior <juniorarruda15@gmail.com>
2024-05-16 15:22:41 -0300

    Adicionado a condição devolução e a contagem de jogos alugados

--- js/app.js
+++ js/app.js
@@ -2,14 +2,34 @@ function alterarStatus(id) {
     let jogoAlugado = document.getElementById(`game-${id}`);
     let imagem = jogoAlugado.querySelector('.dashboard__item__img');
     let botao = jogoAlugado.querySelector('.dashboard__item__button');
+    let nomeDoJogo = jogoAlugado.querySelector('.dashboard__item__name');
+    let jogosAlugados = 0;
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar'
+
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeDoJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+       
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver'
+        jogosAlugados++;
+
     }
+
+    contarEExibirJogosAlugados();
 }
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
